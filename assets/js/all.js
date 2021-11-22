7a52d08337f81973381b798255ef699b678a7602
sylvia-H <sylvie11gaia@gmail.com>
2021-11-23 00:40:56 +0800

    Update 2021-11-22T16:40:44.965Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -288,22 +288,6 @@ function set_returnMarkers() {
   }); // bikeMap.addLayer(returnMarkers);
 }
 "use strict";
-
-// 首頁 lottie 動畫圖
-var logoBike = lottie.loadAnimation({
-  wrapper: lottie_youbike,
-  animType: 'svg',
-  loop: true,
-  autoplay: true,
-  path: './assets/images/youbike.json'
-});
-logoBike.play(); // bodymovin.loadAnimation({
-//     container: document.getElementById('lottie_youbike'), // Required
-//     path: './assets/images/youbike.json', // Required
-//     renderer: 'svg', // Required
-//     loop: true,
-//     autoplay: true
-// })
 "use strict";
 
 // header 驗證
