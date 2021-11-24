1670f673a240744d2f6adb06ed70743adfad323c
sylvia-H <sylvie11gaia@gmail.com>
2021-11-24 17:41:09 +0800

    調整 zoom-in 間距

--- app/assets/js/_route.js
+++ app/assets/js/_route.js
@@ -56,7 +56,7 @@ function getCurrentPos_route(){
       // 載入圖磚
       L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
           attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 19,
+          maxZoom: 17,
           id: 'mapbox/streets-v11',
           tileSize: 512,
           zoomOffset: -1,
@@ -198,13 +198,13 @@ function polyLine(geoData,routeInfo) {
   <div class="fz-4 fw-bold d-flex">
     路線起點：
     <span class="fz-4 fw-normal">
-      ${routeInfo.RoadSectionStart}
+      ${routeInfo.RoadSectionStart?routeInfo.RoadSectionStart:'資料未註明'}
     </span>
   </div>
   <div class="fz-4 fw-bold d-flex">
     路線迄點：
     <span class="fz-4 fw-normal">
-      ${routeInfo.RoadSectionEnd}
+      ${routeInfo.RoadSectionEnd?routeInfo.RoadSectionEnd:'資料未註明'}
     </span>
   </div>`;
     
